0603b02b970fae41617a1084bb11b693270d9473
Anuv225 <anuvindpl05@gmail.com>
2025-04-04 00:55:41 +0530

    feat/added error response and global exception handeling

--- backend/backend/src/main/java/com/event/backend/exception/ErrorResponse.java
+++ backend/backend/src/main/java/com/event/backend/exception/ErrorResponse.java
@@ -1,4 +1,23 @@
 package com.event.backend.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class ErrorResponse {
+
+
+    String message;
+
+    LocalDateTime timestamp;
+
+    String detail;
 }
