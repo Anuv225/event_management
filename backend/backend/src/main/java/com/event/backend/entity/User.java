685244c01dd9ea219b5eb97a6d14cf379085bd81
Anuv225 <anuvindpl05@gmail.com>
2025-04-04 14:58:57 +0530

    feat/frontend setup

--- backend/backend/src/main/java/com/event/backend/entity/User.java
+++ backend/backend/src/main/java/com/event/backend/entity/User.java
@@ -2,7 +2,6 @@ package com.event.backend.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
